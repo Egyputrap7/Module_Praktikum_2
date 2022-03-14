78c9e6e91801140f17f76a5d17e9ee870d657466
Activity_4 <egyputra78@gmail.com>
2022-03-15 04:11:49 +0700

    Activity_4

--- app/src/main/java/com/example/praktikum2/MainActivity.java
+++ app/src/main/java/com/example/praktikum2/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.praktikum2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
     EditText edEmail , edPassword;
 
     String nama,password;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.pendaftaran, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.mnDaftar) {
+            Intent i = new Intent(MainActivity.this, Daftaractivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
